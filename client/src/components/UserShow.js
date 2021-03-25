af6fcb9eccd2899b4e4ae13e2f234a51805bbdb5
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-03-25 17:22:57 -0600

    user show

--- client/src/components/UserShow.js
+++ client/src/components/UserShow.js
@@ -1,11 +1,54 @@
-
+import React, {useEffect, useState, useContext} from 'react'
+import axios from 'axios'
+import { AuthContext } from '../providers/AuthProvider'
+import { Button, Card, Header, Icon, Image } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const UserShow = () => {
+    const [post, setPost] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const {user} = useContext(AuthContext)
+    const [like, setLike] = useState(0)
+
+    useEffect(()=> {
+        getData()
+    },[])
+
+    const getData = async()=> {
+        try{
+            let res = await axios.get(`/users/${user.id}/posts`)
+            setPost(res.data)
+            setLoading(false)
+        } catch(err) {
+            alert('err')
+            setLoading (false)
+        }
+    }
+
+    if(loading) return <p>Loading</p>
+    if(user.posts === 0) return <span>No Posts</span>
+
+    function hitLike(){
+        setLike(prevLike => prevLike +1)
+    }
+
+
+
+
+
     return(
         <div>
-            <h1>
-                User Show
-            </h1>
+            <Header as='h1'>Welcome to {user.nickname}'s page</Header>
+            <div>
+                <Card.Content>
+                    <Button onClick={hitLike}>
+                        {like} 👍
+                    </Button>
+                </Card.Content>
+
+            </div>
+               
+            
         </div>
 
     )
