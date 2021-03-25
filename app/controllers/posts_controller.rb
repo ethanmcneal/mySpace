3450ca73ab0b3aaf4d05503eea56e26098bf2569
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-03-25 15:28:11 -0600

    crud actions for posts complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,55 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  
+
+  def index
+    posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    @post = @user.posts.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = @user.posts.new(post_params)
+
+    if @post.save
+      render json: @post
+    else
+      render json: {errors: @post.errors}, status: 422
+    end    
+  end
+
+  def update
+    @post = @user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: {errors: @post.errors}, status: 422
+    end  
+
+  end
+
+  def destroy
+    @post = @user.posts.find(params[:id])
+    @post.destroy
+    render json: @post
+
+  end
+
+  # def all_post
+  
+  # end
+
+
+  private
+
+  def posts_params
+    params.require(:user).permit(:subject, :body, :image, :likes)
+  end
+
+
 end
